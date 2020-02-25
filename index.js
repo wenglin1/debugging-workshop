459c5e2d9fbb7b6335981ab6d0e08ec93a8745c4
wenglin1 <wenglin@buffalo.edu>
2020-02-25 11:10:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  // const jokeList = document.getElementById('joke-list')
+  // const newJokeLi = document.createElement('li')
+  // const username = document.getElementById('name-input').value
   let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
@@ -16,12 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const jokeList = document.getElementById('joke-list')
+    const newJokeLi = document.createElement('li')
+    const username = document.getElementById('name-input').value
 
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    fetchJoke().then(() => {
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+    })
+    // newJokeLi.innerHTML = `
+    // <span class="username">${username} says:</span> ${joke}
+    // `
+    // jokeList.appendChild(newJokeLi)
   })
 })
